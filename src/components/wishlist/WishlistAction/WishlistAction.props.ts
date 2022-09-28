5f1bfea70a86c913cf0a77b29b368b6f456d3d64
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-09-28 09:01:52 +0200

    Make wishlist action component more reusable

--- src/components/wishlist/WishlistAction/WishlistAction.props.ts
+++ src/components/wishlist/WishlistAction/WishlistAction.props.ts
@@ -2,5 +2,5 @@ import { NftMetadata } from 'store/types'
 
 export interface WishlistActionProps {
   nft: NftMetadata
-  isInWishlist: boolean
+  isInWishlist?: boolean
 }
--- src/components/wishlist/WishlistAction/WishlistAction.tsx
+++ src/components/wishlist/WishlistAction/WishlistAction.tsx
@@ -1,41 +1,38 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { HeartFilled, HeartOutlined } from '@ant-design/icons'
+import Icon, { HeartFilled, HeartOutlined } from '@ant-design/icons'
 import { useAppDispatch } from 'store/hooks'
 import { addToWishlist, removeFromWishlist } from 'store/wishlist'
 import { WishlistActionProps } from './WishlistAction.props'
 
 export const WishlistAction = ({
   nft,
-  isInWishlist,
+  isInWishlist = false,
 }: WishlistActionProps): JSX.Element => {
-  const { image, name, token_address, token_id, token_uri } = nft
   const dispatch = useAppDispatch()
+  const { image, name, token_address, token_id, token_uri } = nft
+  const HeartIcon = !isInWishlist ? HeartOutlined : HeartFilled
+  const iconColor = isInWishlist ? 'red' : ''
 
-  return !isInWishlist ? (
-    <HeartOutlined
-      key='add'
+  return (
+    <Icon
+      aria-label={!isInWishlist ? 'Add to wishlist' : 'Remove from wishlist'}
+      key={!isInWishlist ? 'add' : 'remove'}
+      component={HeartIcon as React.ForwardRefExoticComponent<any>}
+      style={{ color: iconColor }}
       onClick={() =>
         dispatch(
-          addToWishlist({
-            image,
-            name,
-            token_address,
-            token_id,
-            token_uri,
-          })
-        )
-      }
-    />
-  ) : (
-    <HeartFilled
-      key='remove'
-      style={{ color: 'red' }}
-      onClick={() =>
-        dispatch(
-          removeFromWishlist({
-            token_address,
-            token_id,
-          })
+          !isInWishlist
+            ? addToWishlist({
+                image,
+                name,
+                token_address,
+                token_id,
+                token_uri,
+              })
+            : removeFromWishlist({
+                token_address,
+                token_id,
+              })
         )
       }
     />
